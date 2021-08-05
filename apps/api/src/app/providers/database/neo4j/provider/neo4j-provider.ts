c5b0bd720a4f8fee2123e6f470b38b2db6764fd8
Emil Sroka <srokaemil@gmail.com>
2021-08-05 21:50:34 +0200

    dev: implement Neo4jProvider

--- apps/api/src/app/providers/database/neo4j/provider/neo4j-provider.ts
+++ apps/api/src/app/providers/database/neo4j/provider/neo4j-provider.ts
@@ -1,10 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationShutdown } from '@nestjs/common';
 import { Observable } from 'rxjs';
-import { Record } from 'neo4j-driver';
+import { auth, driver, Record } from 'neo4j-driver';
 
 @Injectable()
-export class Neo4jProvider {
+export class Neo4jProvider implements OnApplicationShutdown {
+  private _driver = driver(
+    process.env.DB_URL,
+    auth.basic(process.env.DB_LOGIN, process.env.DB_PASSWORD)
+  );
+  private _session = this._driver.rxSession();
+
   query(query: string): Observable<Record> {
-    return new Observable();
+    return this._session.run(query).records();
+  }
+
+  onApplicationShutdown(): void {
+    this._session.close();
+    this._driver.close();
   }
 }
