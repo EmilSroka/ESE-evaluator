ad821273e0fc4a4366d8bf41ac27d28586d6f5c7
Emil Sroka <srokaemil@gmail.com>
2021-08-06 22:23:46 +0200

    chore: setup graphql

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { join } from 'path';
 import { Neo4jModule } from './providers/database/neo4j/neo4j.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TmpResolver } from './graphql/tmp.resolver';
 
 @Module({
-  imports: [Neo4jModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/app/graphql/schema.gql'),
+    }),
+    Neo4jModule,
+  ],
+  providers: [TmpResolver],
 })
 export class AppModule {}
