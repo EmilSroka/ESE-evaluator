16fceab35db85047fcd01c86ef086a9e29dcb241
Emil Sroka <srokaemil@gmail.com>
2021-08-07 16:25:43 +0200

    dev: implement language service

--- apps/api/src/app/feature/translation/services/language/language.service.ts
+++ apps/api/src/app/feature/translation/services/language/language.service.ts
@@ -1,13 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { Neo4jProvider } from '../../../../providers/database/neo4j/provider/neo4j-provider';
 import { LanguageObject } from '../../models/language.model';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+import { filter, map, reduce } from 'rxjs/operators';
+
+const VARIABLE = 'l';
+const PROPERTIES = ['englishName', 'tag', 'ownName'];
+const QUERY = `MATCH (${VARIABLE}:Language) RETURN l`;
 
 @Injectable()
 export class LanguageService {
   constructor(private _neo4j: Neo4jProvider) {}
 
   get(): Observable<LanguageObject[]> {
-    return of([]);
+    return this._neo4j.query(QUERY).pipe(
+      map((record) => record.get(VARIABLE).properties),
+      filter(
+        (props) =>
+          PROPERTIES.filter((prop) => props[prop] != null).length ===
+          PROPERTIES.length
+      ),
+      map((props) => {
+        const language = new LanguageObject();
+        for (const property of PROPERTIES) {
+          language[property] = props[property];
+        }
+        return language;
+      }),
+      reduce((acc, lang) => {
+        acc.push(lang);
+        return acc;
+      }, [])
+    );
   }
 }
