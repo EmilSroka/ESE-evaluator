972ad6bcfc680d99a87af0ce036eec04c9654795
Emil Sroka <srokaemil@gmail.com>
2021-08-16 19:25:37 +0200

    dev: implement default language endpoint

--- apps/api/src/app/graphql/translations/resolvers/default-language/default-language.resolver.ts
+++ apps/api/src/app/graphql/translations/resolvers/default-language/default-language.resolver.ts
@@ -1,14 +1,44 @@
 import { Query, Resolver } from '@nestjs/graphql';
 import { Language } from '../../models/language.model';
 import { LanguageService } from '../../../../feature/translation/services/language/language.service';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+import { Inject, InternalServerErrorException, Logger } from '@nestjs/common';
+import { DEFAULT_LANGUAGE_TOKEN } from './default-language.token';
 
 @Resolver(of => Language)
 export class DefaultLanguageResolver {
-  constructor(private languages: LanguageService) {}
+  constructor(
+    @Inject(DEFAULT_LANGUAGE_TOKEN) private defaultLang: string,
+    private languages: LanguageService,
+    private logger: Logger,
+  ) {}
 
   @Query(returns => Language)
   defaultLanguage(): Observable<Language> {
-    return of();
+    return new Observable<Language>(subscriber => {
+      this.languages.get().subscribe({
+        next: languages => {
+          if (languages.length === 0) {
+            this.logger.error(
+              'DefaultLanguageResolver: Languages list returned from LanguageService is empty',
+            );
+            subscriber.error(
+              new InternalServerErrorException('Internal server error'),
+            );
+          } else if (!languages.find(({ tag }) => tag === this.defaultLang)) {
+            this.logger.warn(
+              `DefaultLanguageResolver: value '${this.defaultLang}' stored under DEFAULT_LANGUAGE_TOKEN is not present in languages list`,
+            );
+            subscriber.next(languages[0]);
+            subscriber.complete();
+          } else {
+            subscriber.next(
+              languages.find(({ tag }) => tag === this.defaultLang),
+            );
+            subscriber.complete();
+          }
+        },
+      });
+    });
   }
 }
