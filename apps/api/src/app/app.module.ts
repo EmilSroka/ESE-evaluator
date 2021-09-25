c0b75360a89337dbceb65ed9ca1132746f9ac4ce
Emil Sroka <srokaemil@gmail.com>
2021-09-25 22:57:25 +0200

    dev: add core module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { Neo4jModule } from './providers/database/neo4j/neo4j.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { GqlApiModule } from './graphql/gql.api.module';
 import { AuthModule } from './feature/auth/auth.module';
+import { CoreModule } from './core/core.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/app/graphql/schema.gql'),
     }),
+    CoreModule,
     Neo4jModule,
     GqlApiModule,
     AuthModule,
